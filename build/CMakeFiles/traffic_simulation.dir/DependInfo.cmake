f9d9ee9b520caf60630761478ecb5f124c07713c
David Francis <v3w63mrg@verizon.net>
2022-01-23 11:37:46 -0500

    Commit for Project submission 01/23/2022

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/david_francis/projects/Cpp_Cts/src/Graphics.cpp" "/home/david_francis/projects/Cpp_Cts/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/home/david_francis/projects/Cpp_Cts/src/Intersection.cpp" "/home/david_francis/projects/Cpp_Cts/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/home/david_francis/projects/Cpp_Cts/src/Street.cpp" "/home/david_francis/projects/Cpp_Cts/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/home/david_francis/projects/Cpp_Cts/src/TrafficLight.cpp" "/home/david_francis/projects/Cpp_Cts/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/home/david_francis/projects/Cpp_Cts/src/TrafficObject.cpp" "/home/david_francis/projects/Cpp_Cts/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/home/david_francis/projects/Cpp_Cts/src/TrafficSimulator-Final.cpp" "/home/david_francis/projects/Cpp_Cts/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/home/david_francis/projects/Cpp_Cts/src/Vehicle.cpp" "/home/david_francis/projects/Cpp_Cts/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,58 +1,118 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 #include "TrafficLight.h"
 
-/* Implementation of class "MessageQueue" */
+using namespace std::chrono_literals;
 
-/* 
+// Implementation of class "MessageQueue"
+
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // FP.5a
+    //Lock prior to accessing
+    std::unique_lock<std::mutex> uLock(_mutex);
+    //Wait for new message
+    _cond.wait(uLock, [this] { return !_queue.empty(); });
+    //Use move semantics to remove message from queue
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    // FP.4a
+    //Lock prior to accessing
+    std::lock_guard<std::mutex> uLock(_mutex);
+    //Remove old messages
+    _queue.clear();
+    //Add new message to queue
+    _queue.emplace_back(std::move(msg));
+    _cond.notify_one();
+    std::cout << " Message " << msg << " has been added to the queue" << std::endl;
 }
-*/
 
-/* Implementation of class "TrafficLight" */
+// Implementation of class "TrafficLight"
 
-/* 
 TrafficLight::TrafficLight()
 {
+    //Initialize phase of traffic light
     _currentPhase = TrafficLightPhase::red;
 }
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
+    // FP.5b
+    while (true)
+    {  
+        //Test phase of traffic light message
+        if (_message.receive() == TrafficLightPhase::green)
+        {
+            //If green light received, notify console and exit loop
+            std::cout << " Green Light!" << std::endl;
+            return;
+        }
+    }
 }
 
-TrafficLightPhase TrafficLight::getCurrentPhase()
+TrafficLight::TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
 }
 
+
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // FP.2b
+    //Start looping through traffic light phases
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    //Added this temporary variable for the if condition below
+    TrafficLightPhase tempPhase;
+    //Range of cycle time in Milliseconds
+    long lowCycle = 4000;
+    long highCycle = 6000;
+    //Select random cycle time within range
+    long randomCycle = lowCycle + rand() % (highCycle - lowCycle);
+    std::cout << " Light cycle time = " << randomCycle << std::endl;
+    //Initialize time of "This" loop cycle
+    auto timeThis = std::chrono::high_resolution_clock::now();
+    long cycleDuration = 0;
+    while (true)
+    {
+        //Time of "Previous" loop cycle
+        auto timeLast = timeThis;
+        timeThis = std::chrono::high_resolution_clock::now();
+        //Time between loop cycles
+        auto loopDuration = std::chrono::duration_cast<std::chrono::milliseconds>(timeThis - timeLast).count();
+        //Increment cycle time by loop time
+        cycleDuration += loopDuration;
+        if (cycleDuration > randomCycle)
+        {
+            //If duration is complete, cycle traffic light phase
+            tempPhase = _currentPhase;
+            if (tempPhase == red)
+            {
+                _currentPhase = green;
+            }
+            else
+            {
+                _currentPhase = red;
+            }
+            //Use move semantics to send update method to message queue
+            _message.send(std::move(_currentPhase));
+            //Reset cycle duration for next phase change
+            cycleDuration = 0;
+        }
+        //Delay
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 }
-
-*/
